7a7a3980f9befb8b8f6e86eef6eab0fec6f76230
NABEEL-AHMED-JAMIL <nabeel.amd93@gmail.com>
2017-07-02 12:02:38 +0500

    xyz

--- src/main/java/com/ballistic/hospital/HospitalBackEndApplication.java
+++ src/main/java/com/ballistic/hospital/HospitalBackEndApplication.java
@@ -8,16 +8,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 @SpringBootApplication
 @Configuration
-@EnableWebMvc
 @ComponentScan(basePackages = "com.ballistic.hospital")
 public class HospitalBackEndApplication implements UserDetailsService {
 
