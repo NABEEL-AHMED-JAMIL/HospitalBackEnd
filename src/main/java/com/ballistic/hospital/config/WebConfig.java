2609f21ce76a1f36d34c7cc8816e4262adf05d0f
NABEEL-AHMED-JAMIL <nabeel.amd93@gmail.com>
2017-07-23 11:53:26 +0500

    config cros

---
+++ src/main/java/com/ballistic/hospital/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.ballistic.hospital.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Lycus 01 on 7/16/2017.
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("PUT", "DELETE", "GET", "POST", "OPTIONS","HEAD", "DELETE", "PATCH");
+        //.allowedOrigins("http://localhost:4200");
+    }
+}
