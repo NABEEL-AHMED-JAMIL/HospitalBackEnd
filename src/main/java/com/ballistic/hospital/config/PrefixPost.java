5ab5351ff3ba23af31f98101089061fa59c1d9dc
Nabeel Ahemd <nabeel.amd93@gmail.com>
2017-10-16 12:33:30 +0500

    Create PrefixPost.java

---
+++ src/main/java/com/ballistic/hospital/config/PrefixPost.java
@@ -0,0 +1,31 @@
+package com.ballistic.hospital.config
+
+public class PrefixPost {
+	
+	private String prefixProp;
+	private String suffixProp;
+	
+	public String getPrefixProp() {
+		return prefixProp
+	}
+	
+	public void setPrefixProp(String prefixProp) {
+		this.prefixProp = prefixProp;
+	}
+	
+	public String getSuffixProp() {
+		return suffixProp;
+	}
+	
+	public void setSuffixProp(String suffixProp) {
+		this.suffixProp = suffixProp;
+	}
+	
+	public void sayHello() {
+		System.out.println(prefixProp + "!");
+	}
+	
+	public void sayGoodBye() {
+		System.out.println(suffixProp + "!");
+	}
+}
